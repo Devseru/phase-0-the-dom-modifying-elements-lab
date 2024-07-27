106a6aa342ff4644b2e7efce3f2d850e43d23b00
Devseru <serushikoli65@gmail.com>
2024-07-27 18:48:39 +0300

    completing the DOM

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'EMMANUEL is the champion';
+
+document.body.appendChild(newHeader);
